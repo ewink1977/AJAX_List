866c57443b5d0bda5b2e3a8dba31e762b5fbcac2
Erin Winking <ewink@yahoo.com>
2021-02-02 23:32:23 -0800

    Database seeded, display working properly.

--- AJAXPageApp/urls.py
+++ AJAXPageApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.displayMainPage, name = 'displayMain'),
+    
 ]
--- AJAXPageApp/views.py
+++ AJAXPageApp/views.py
@@ -1,4 +1,14 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
+from .models import User
 
 def displayMainPage(request):
-    return render(request, 'html/main.html')
+    users = User.objects.all()
+    paginator = Paginator(users, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj
+    }
+    return render(request, 'html/main.html', context)
